36dc4a8a27e80280f4f657c0183d630fb13cd75d
Sebastian Veggerby Rasmussen <SFU@kmd.dk>
2023-03-16 10:06:08 +0100

    Add project files.

---
+++ Objekter Opgaver-16-03-2023/Bankkonto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objekter_Opgaver_16_03_2023
+{
+    public class Bankkonto
+    {
+        private  decimal saldo { get;  set; } = 1000; 
+
+        public void Skrivsaldo()
+        {
+            Console.Write(this.saldo.ToString());
+        }
+
+        public void withdrawl(decimal input)
+        {
+            if(input < 0)
+            {
+                Console.WriteLine("du kan ikke udtrække minus");
+            }
+            if (input > 0)
+            {
+
+
+                if (saldo - input < 0)
+                {
+                    Console.WriteLine("du har ikke nok dollares til at hæve makker");
+                }
+                if (saldo - input > 0)
+                {
+                    Console.WriteLine($"du har hævet {input} fra din konto og du har {saldo - input} tilbage");
+                    saldo -= input;
+                }
+            }
+            
+        }
+        public Bankkonto(decimal saldo)
+        {
+           this.saldo = saldo;
+        }
+
+        public  void deposit(decimal input)
+        {
+            if(input < 0)
+            {
+                Console.WriteLine("du kan ikke sætte minus ind");
+            }
+            else { Console.WriteLine($"du har sat {input} ind på din konto og den har en ny slado af {saldo + input}"); saldo += input;
+            }
+        }
+
+    }
+}
---
+++ Objekter Opgaver-16-03-2023/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Objekter_Opgaver_16_03_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bankkonto bk1 = new Bankkonto(2000);
+            Console.WriteLine("Velkommen til bankontoen skriv venligst et tal fra 1 - 3 ");
+
+            while (true)
+            {
+                Console.WriteLine("1 for indsæt\n2 for udtærk\n3 for at se kontosaldo");
+               var input =Convert.ToInt32( Console.ReadLine());
+                
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("skriv mængde du vil indsætte");
+                        var saldoinput = Convert.ToDecimal( Console.ReadLine());
+                        bk1.deposit(saldoinput);
+                    break;
+                    case 2:
+                        Console.WriteLine("skriv mængde du vil udtrække");
+                        var saldoout = Convert.ToDecimal(Console.ReadLine());
+                        bk1.withdrawl(saldoout);
+                        break;
+                    case 3:
+                        Console.Write($"du har ");
+                        bk1.Skrivsaldo(); 
+                        Console.Write( " på kontoen");
+                        Console.WriteLine();
+                        break;
+                    default: 
+                    break;
+                }
+            }
+        }
+    }
+}
